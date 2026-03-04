430701c51c667a4c1030bab18ea802cf0c43ce82
Praveen Thilanka <praveenethipola@gmail.com>
2026-03-04 23:58:47 +0530

    Configured Vue Router and Pinia

---
+++ src/router/index.ts
@@ -0,0 +1,17 @@
+import { createRouter, createWebHistory } from "vue-router"
+import HomeView from "../views/HomeView.vue"
+import ProductDetailView from "../views/ProductDetailView.vue"
+import CartView from "../views/CartView.vue"
+
+const routes = [
+  { path: "/", component: HomeView },
+  { path: "/product/:id", component: ProductDetailView },
+  { path: "/cart", component: CartView }
+]
+
+const router = createRouter({
+  history: createWebHistory(),
+  routes
+})
+
+export default router
